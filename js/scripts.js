20952925755062fe5569458c55677fbd71c60ff0
Justyna Kaczorowska <js.kaczorowska@gmail.com>
2018-02-28 21:44:58 +0100

    Add var and cosole.log

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+var a = 4,
+	b = 3,
+	value = (a * a) - (2 * a * b) - (b * b);
+console.log("The result of the action contained in'value' = " + value);
+alert("The result of the action contained in'value' = " + value);
