dcb80cb24668239c9b011ae7915f01c9ff293f17
Justyna Kaczorowska <js.kaczorowska@gmail.com>
2018-02-28 21:50:20 +0100

    Change content in console.log

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 var a = 4,
 	b = 3,
 	value = (a * a) - (2 * a * b) - (b * b);
-console.log("The result of the action contained in'value' = " + value);
-alert("The result of the action contained in'value' = " + value);
+console.log("The result of the action = " + value);
+alert("The result of the action  = " + value);
