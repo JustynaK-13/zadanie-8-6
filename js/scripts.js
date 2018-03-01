ccda6994f27abb8d192e58fc92159209e42ef1e0
Justyna Kaczorowska <js.kaczorowska@gmail.com>
2018-03-01 13:33:10 +0100

    Change content

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,14 @@
 var a = 4,
-	b = 3,
-	value = (a * a) - (2 * a * b) - (b * b);
+b = 3,
+value = (a * a) - (2 * a * b) - (b * b);
 console.log("The result of the action = " + value);
 alert("The result of the action  = " + value);
 
 if (value < 0) {
-    console.log('true');
-} else if (value > 0) {
-    console.log('false');
-} else {
-   console.log('Wynik ujemny');
+    console.log('Wynik ujemny');
 }
 /*
-if (-17 == 0){
-	console.log('false')
-}
-else (-17 == 0){
-	console.log('false')
+if (value == 0) {
+console.log('false');
 }
 */
