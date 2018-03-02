7661be4634317342e233b27233818440c7350c80
Justyna Kaczorowska <js.kaczorowska@gmail.com>
2018-03-02 09:26:26 +0100

    Change content

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,13 @@
 var a = 4,
-b = 3,
-value = (a * a) - (2 * a * b) - (b * b);
+  b = 3,
+  value = (a * a) - (2 * a * b) - (b * b);
 console.log("The result of the action = " + value);
 alert("The result of the action  = " + value);
 
 if (value < 0) {
     console.log('Wynik ujemny');
+} if (value > 0) {
+    console.log ('Wynik dodatni');
 } else if (value == 0) {
 	console.log('false');
 }
